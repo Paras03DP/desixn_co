2692ad7c88e2b93af6112fe6349ae8c72ce811c5
Parasayer13 <paras.ayer13@gmail.com>
2023-05-27 01:50:16 +0545

    get&post

--- api/routes/student.js
+++ api/routes/student.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const Student = require('../model/student');
+const mongoose = require('mongoose');
 
 router.get('/', (req,res,next)=>{
-    res.status(200).json({
-        msg: 'this is student get request'
+    Student.find()
+    .then(result=>{
+        res.status(200).json({
+            studentData:result
+        });
     })
+    .catch(err=>{
+        console.log(err);
+        res.statusMessage(500).json({
+            error:err
+        })
+    });
 })
 
 router.post('/', (req,res,next)=>{
-    console.log(req.body);
+    const student = new Student({
+    _id:new mongoose.Types.ObjectId,
+    name:req.body.name,
+    email:req.body.email,
+    phone:req.body.number,
+    gender:req.body.gender
+    })
+
+    student.save()
+    .then(result=>{
+        console.log(result);
+        res.status(200).json({
+            newStudent:result
+        });
+    })
+
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
+    });
 })
 
 module.exports = router;
