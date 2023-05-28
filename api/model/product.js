398a65e88e9e33f305d5e28c649c41558e60d5af
Parasayer13 <paras.ayer13@gmail.com>
2023-05-28 23:38:35 +0545

    putrequest

--- api/model/product.js
+++ api/model/product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-productSchema = new mongoose.Schema({
+productSchema = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     code:String,
     title:String,
